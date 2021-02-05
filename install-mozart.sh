1a74debbb67c85bb1d62b0bf6081aeee8bbd7fbd
George Chang <george.w.chang@jpl.nasa.gov>
2021-02-05 14:03:01 -0800

    Previous commit didn't get catch the elastic search installers

--- install-mozart.sh
+++ install-mozart.sh
@@ -68,7 +68,7 @@ if [ ! -d $mod_dir ]; then
   $git_cmd clone $git_loc $mod_dir
   pushd .
   cd $mod_dir
-  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-26 23:59:59" master`
+  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-25 23:59:59" master`
   $git_cmd reset --hard HEAD
   popd
 fi
@@ -87,7 +87,7 @@ if [ ! -d $mod_dir ]; then
   $git_cmd clone $git_loc $mod_dir
   pushd .
   cd $mod_dir
-  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-26 23:59:59" master`
+  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-25 23:59:59" master`
   $git_cmd reset --hard HEAD
   popd
 fi
@@ -106,7 +106,7 @@ if [ ! -d $mod_dir ]; then
   $git_cmd clone $git_loc $mod_dir
   pushd .
   cd $mod_dir
-  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-26 23:59:59" master`
+  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-25 23:59:59" master`
   $git_cmd reset --hard HEAD
   popd
 fi
@@ -125,7 +125,7 @@ if [ ! -d $mod_dir ]; then
   $git_cmd clone $git_loc $mod_dir
   pushd .
   cd $mod_dir
-  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-26 23:59:59" master`
+  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-25 23:59:59" master`
   $git_cmd reset --hard HEAD
   popd
 fi
@@ -144,7 +144,7 @@ if [ ! -d $mod_dir ]; then
   $git_cmd clone $git_loc $mod_dir
   pushd .
   cd $mod_dir
-  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-26 23:59:59" master`
+  $git_cmd checkout `$git_cmd rev-list -n 1 --first-parent --before="2020-02-25 23:59:59" master`
   $git_cmd reset --hard HEAD
   popd
 fi
